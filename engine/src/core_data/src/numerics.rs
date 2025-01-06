17d5cb7dcabbae03730230c9aad3ce49278b3e9a
Derek Thurn <derek@fake.email>
2025-01-06 07:13:11 -0800

    derive more

--- engine/src/core_data/src/numerics.rs
+++ engine/src/core_data/src/numerics.rs
@@ -12,17 +12,89 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use derive_more::{
+    Add, AddAssign, Display, Div, DivAssign, From, Into, Mul, MulAssign, Sub, SubAssign, Sum,
+};
 use serde::{Deserialize, Serialize};
 
 /// A resource used to pay for cards & abilities.
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(
+    Debug,
+    Display,
+    Copy,
+    Clone,
+    PartialEq,
+    Eq,
+    Ord,
+    PartialOrd,
+    Hash,
+    From,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Sum,
+    AddAssign,
+    SubAssign,
+    MulAssign,
+    DivAssign,
+    Into,
+    Serialize,
+    Deserialize,
+)]
 pub struct Energy(pub u64);
 
 /// Represents the 'power' of characters; the primary way in which players earn
 /// victory points.
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(
+    Debug,
+    Display,
+    Copy,
+    Clone,
+    PartialEq,
+    Eq,
+    Ord,
+    PartialOrd,
+    Hash,
+    From,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Sum,
+    AddAssign,
+    SubAssign,
+    MulAssign,
+    DivAssign,
+    Into,
+    Serialize,
+    Deserialize,
+)]
 pub struct Spark(pub u64);
 
 /// Victory points. Enable the player to win the game.
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(
+    Debug,
+    Display,
+    Copy,
+    Clone,
+    PartialEq,
+    Eq,
+    Ord,
+    PartialOrd,
+    Hash,
+    From,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Sum,
+    AddAssign,
+    SubAssign,
+    MulAssign,
+    DivAssign,
+    Into,
+    Serialize,
+    Deserialize,
+)]
 pub struct Points(pub u64);
