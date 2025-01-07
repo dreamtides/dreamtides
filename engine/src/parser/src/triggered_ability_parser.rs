8f76395d30587cbd5e0e1ca3674abf6bd0184b3a
Derek Thurn <derek@fake.email>
2025-01-07 10:48:04 -0800

    remove keyword

--- engine/src/parser/src/triggered_ability_parser.rs
+++ engine/src/parser/src/triggered_ability_parser.rs
@@ -13,20 +13,17 @@
 // limitations under the License.
 
 use ability_data::triggered_ability::TriggeredAbility;
-use chumsky::{text, Parser};
+use chumsky::prelude::*;
+use chumsky::Parser;
 
 use crate::parser_utils::{phrase, ErrorType};
 use crate::{effect_parser, trigger_event_parser};
 
 pub fn parser<'a>() -> impl Parser<'a, &'a str, TriggeredAbility, ErrorType<'a>> {
-    trigger_keyword()
+    choice((phrase("whenever"), phrase("when")))
         .ignore_then(trigger_event_parser::parser())
         .then_ignore(phrase(","))
         .then(effect_parser::parser())
         .then_ignore(phrase("."))
         .map(|(event, effects)| TriggeredAbility::new(event, effects))
-}
-
-fn trigger_keyword<'a>() -> impl Parser<'a, &'a str, &'a str, ErrorType<'a>> {
-    text::keyword("whenever").or(text::keyword("when"))
 }
