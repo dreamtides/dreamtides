d5572b69a350d1d8940792f99f6011120db91ea5
Derek Thurn <derek@fake.email>
2025-01-11 14:40:42 -0800

    install fontawesome

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
-import { Button, Chip } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faBug } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <Button color="primary">Button</Button>
-        <Chip>Chip</Chip>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
+      <Button
+        isIconOnly
+        aria-label="Like"
+        color="primary"
+        variant="bordered"
+        size="lg"
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+        <FontAwesomeIcon icon={faBars} />
+      </Button>
+      <Button
+        isIconOnly
+        aria-label="Like"
+        color="primary"
+        variant="bordered"
+        size="lg"
+      >
+        <FontAwesomeIcon icon={faBug} />
+      </Button>
     </main>
   );
 }
