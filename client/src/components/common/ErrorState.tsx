c217eda68eeebfbccef5e1e94faeabf477b71c07
Derek Thurn <derek@fake.email>
2025-01-12 20:43:26 -0800

    use swr

---
+++ client/src/components/common/ErrorState.tsx
@@ -0,0 +1,5 @@
+type ErrorStateProps = {};
+
+export function ErrorState({}: ErrorStateProps) {
+    return <div>Error</div>;
+}
--- client/src/screens/battle/BattleScreen.tsx
+++ client/src/screens/battle/BattleScreen.tsx
@@ -1,16 +1,44 @@
+import { BattleView, ClientBattleId, commands } from "../../bindings";
+import { ErrorState } from "../../components/common/ErrorState";
+import { Loading } from "../../components/common/Loading";
 import NavigationBar from "../../components/common/NavigationBar";
 import Battlefield from "./Battlefield";
 import BattlePlayerStatus from "./BattlePlayerStatus";
 import EnemyHand from "./EnemyHand";
 import UserHand from "./UserHand";
+import useSWR from "swr";
+
+type BattleFetchResult = { battle: BattleView } | { error: Error } | { isLoading: boolean };
+
+export function useBattle(id: ClientBattleId): BattleFetchResult {
+  const { data, error, isLoading } = useSWR(id, commands.fetchBattle);
+
+  if (isLoading) {
+    return { isLoading: true };
+  }
+  else if (error || data == null) {
+    return { error };
+  }
+  else {
+    return { battle: data };
+  }
+}
 
 type BattleScreenProps = {};
 
-export default function BattleScreen({}: BattleScreenProps) {
+export default function BattleScreen({ }: BattleScreenProps) {
+  const result = useBattle("123");
+  if ("isLoading" in result) {
+    return <Loading />;
+  } else if ("error" in result) {
+    return <ErrorState />;
+  }
+
   return (
     <div className="flex flex-col h-screen w-screen">
       <NavigationBar>
-        <EnemyHand />
+        {result.battle.id}
+        {/* <EnemyHand /> */}
       </NavigationBar>
       <BattlePlayerStatus />
       <Battlefield />
