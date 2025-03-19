d9cfe4f3c70a6f01843be5bd0b4e4b00bb07e5ff
Derek Thurn <derek@fake.email>
2025-03-19 12:57:32 -0700

    judgment animation effects

--- client/Assets/Dreamcaller/Services/JudgmentService.cs
+++ client/Assets/Dreamcaller/Services/JudgmentService.cs
@@ -2,8 +2,10 @@
 
 using System.Collections;
 using DG.Tweening;
+using Dreamcaller.Components;
 using Dreamcaller.Schema;
 using Dreamcaller.Utils;
+using Graphy.Runtime.UI;
 using UnityEngine;
 
 namespace Dreamcaller.Services
@@ -13,25 +15,27 @@ namespace Dreamcaller.Services
     [SerializeField] float _durationMultiplier = 1f;
     [SerializeField] float _shakeStrength = 0.5f;
     [SerializeField] int _shakeVibrato = 10;
+    [SerializeField] AudioClip _startSound = null!;
+    [SerializeField] AudioClip _windUpSound = null!;
+    [SerializeField] AudioClip _noPointsSound = null!;
+    [SerializeField] AudioClip _pointsSound = null!;
+    [SerializeField] TimedEffect _hitEffectPrefab = null!;
 
     public IEnumerator HandleDisplayJudgmentCommand(DisplayJudgmentCommand displayJudgment)
     {
       var sequence = TweenUtils.Sequence("DisplayJudgment");
-      var actorSparkTotal = displayJudgment.Player switch
-      {
-        PlayerName.User => Registry.Layout.UserStatusDisplay.TotalSpark.transform,
-        PlayerName.Enemy => Registry.Layout.EnemyStatusDisplay.TotalSpark.transform,
-        _ => throw Errors.UnknownEnumValue(displayJudgment.Player)
-      };
-      var opponentSparkTotal = displayJudgment.Player switch
-      {
-        PlayerName.User => Registry.Layout.EnemyStatusDisplay.TotalSpark.transform,
-        PlayerName.Enemy => Registry.Layout.UserStatusDisplay.TotalSpark.transform,
-        _ => throw Errors.UnknownEnumValue(displayJudgment.Player)
-      };
+      var actorIsUser = displayJudgment.Player == PlayerName.User;
+      var actorScoredPoints = displayJudgment.NewScore != null;
+      var actorSparkTotal = actorIsUser ?
+          Registry.Layout.UserStatusDisplay.TotalSpark.transform :
+          Registry.Layout.EnemyStatusDisplay.TotalSpark.transform;
+      var opponentSparkTotal = actorIsUser ?
+          Registry.Layout.EnemyStatusDisplay.TotalSpark.transform :
+          Registry.Layout.UserStatusDisplay.TotalSpark.transform;
       Vector3 actorOriginalPos = actorSparkTotal.position;
       Vector3 opponentOriginalPos = opponentSparkTotal.position;
 
+      Registry.SoundService.Play(_startSound);
       var actorUpPosition = new Vector3(
         actorOriginalPos.x + 1f,
         actorOriginalPos.y + 3,
@@ -49,23 +53,36 @@ namespace Dreamcaller.Services
       sequence.Append(actorSparkTotal.DOMove(new Vector3(
         actorUpPosition.x,
         actorUpPosition.y,
-        actorUpPosition.z - 1f
+        actorUpPosition.z + (actorIsUser ? -1f : 1f)
       ), _durationMultiplier * 0.1f));
+      sequence.AppendCallback(() => Registry.SoundService.Play(_windUpSound));
 
       // Rapidly move back towards opponent
       sequence.Append(actorSparkTotal.DOMove(new Vector3(
         actorUpPosition.x,
         actorUpPosition.y,
-        actorUpPosition.z + 1f
+        actorUpPosition.z + (actorIsUser ? 1f : -1f)
       ), _durationMultiplier * 0.05f));
+      sequence.AppendCallback(() =>
+      {
+        var hit = Registry.AssetPoolService.Create(_hitEffectPrefab, actorSparkTotal.position);
+        var rotation = Quaternion.LookRotation(transform.position - Registry.Layout.MainCamera.transform.position);
+        hit.transform.rotation = rotation;
+        hit.transform.localScale = 5f * Vector3.one;
+        Registry.SoundService.Play(actorScoredPoints ? _pointsSound : _noPointsSound);
+      });
 
       // Add shake effect after collision
-      var shakeActor = displayJudgment.NewScore == null ? actorSparkTotal : opponentSparkTotal;
+      var shakeActor = actorScoredPoints ? opponentSparkTotal : actorSparkTotal;
       sequence.Append(shakeActor.DOShakePosition(_durationMultiplier * 0.1f, _shakeStrength, _shakeVibrato));
 
       // Animate both objects returning to their original positions
       sequence.Append(actorSparkTotal.DOMove(actorOriginalPos, _durationMultiplier * 0.1f));
       sequence.Join(opponentSparkTotal.DOMove(opponentOriginalPos, _durationMultiplier * 0.1f));
+      sequence.AppendCallback(() =>
+      {
+        Registry.SoundService.Play(_startSound);
+      });
 
       yield return sequence.WaitForCompletion();
     }
