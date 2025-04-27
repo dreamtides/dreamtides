9afe52deb016f89a57804ecb3a8f88787163a597
Derek Thurn <derek@fake.email>
2025-04-27 14:01:14 -0700

    swap stack direction

--- client/Assets/Dreamtides/Layout/StackingObjectLayout.cs
+++ client/Assets/Dreamtides/Layout/StackingObjectLayout.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 namespace Dreamtides.Layout
 {
-  public class LeftStackingObjectLayout : StandardObjectLayout
+  public class StackingObjectLayout : StandardObjectLayout
   {
     [SerializeField] float _offset;
+    [SerializeField] bool _stackRight;
 
     protected override Vector3 CalculateObjectPosition(int index, int count)
     {
-      var xOffset = (index - (count - 1)) * _offset;
+      var xOffset = _stackRight
+          ? index * _offset
+          : (index - (count - 1)) * _offset;
       return new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z);
     }
 
