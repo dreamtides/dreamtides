837813ce6decbd94622fb33943acd5275a672036
Derek Thurn <derek@fake.email>
2025-05-23 09:42:01 -0700

    calculate current stack type

--- rules_engine/src/display/src/rendering/positions.rs
+++ rules_engine/src/display/src/rendering/positions.rs
@@ -1,6 +1,8 @@
 use battle_state::battle::battle_state::BattleState;
-use battle_state::battle::card_id::CardId;
+use battle_state::battle::card_id::{CardId, CardIdType};
+use battle_state::battle_cards::stack_card_state::StackCardTargets;
 use battle_state::battle_cards::zone::Zone;
+use battle_state::prompt_types::prompt_data::PromptType;
 use core_data::types::PlayerName;
 use display_data::object_position::{ObjectPosition, Position, StackType};
 
@@ -17,7 +19,7 @@ pub fn calculate(
         Zone::Hand => Position::InHand(player),
         Zone::Deck => Position::InDeck(player),
         Zone::Battlefield => Position::OnBattlefield(player),
-        Zone::Stack => Position::OnStack(StackType::Default),
+        Zone::Stack => Position::OnStack(current_stack_type(builder, battle)),
         Zone::Void => Position::InVoid(player),
         Zone::Banished => Position::InBanished(player),
     };
@@ -63,5 +65,56 @@ pub fn controller_and_zone(battle: &BattleState, card_id: CardId) -> ControllerA
             }
         }
         panic!("Card not found in any zone: {:?}", card_id)
+    }
+}
+
+fn current_stack_type(builder: &ResponseBuilder, battle: &BattleState) -> StackType {
+    let display_player = builder.display_for_player();
+    let mut targeting_user_battlefield = false;
+    let mut targeting_enemy_battlefield = false;
+
+    for stack_card in battle.cards.all_cards_on_stack() {
+        if let Some(StackCardTargets::Character(character_id)) = &stack_card.targets {
+            if battle.cards.contains_card(display_player, character_id.card_id(), Zone::Battlefield)
+            {
+                targeting_user_battlefield = true;
+            } else if battle.cards.contains_card(
+                display_player.opponent(),
+                character_id.card_id(),
+                Zone::Battlefield,
+            ) {
+                targeting_enemy_battlefield = true;
+            }
+        }
+    }
+
+    if let Some(ref prompt) = battle.prompt {
+        if let PromptType::ChooseCharacter { ref valid } = prompt.prompt_type {
+            for character_id in valid.iter() {
+                if battle.cards.contains_card(
+                    display_player,
+                    character_id.card_id(),
+                    Zone::Battlefield,
+                ) {
+                    targeting_user_battlefield = true;
+                } else if battle.cards.contains_card(
+                    display_player.opponent(),
+                    character_id.card_id(),
+                    Zone::Battlefield,
+                ) {
+                    targeting_enemy_battlefield = true;
+                }
+            }
+        }
+    }
+
+    if targeting_user_battlefield && targeting_enemy_battlefield {
+        StackType::TargetingBothBattlefields
+    } else if targeting_user_battlefield {
+        StackType::TargetingUserBattlefield
+    } else if targeting_enemy_battlefield {
+        StackType::TargetingEnemyBattlefield
+    } else {
+        StackType::Default
     }
 }
