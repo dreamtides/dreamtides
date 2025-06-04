0c2f422a8f0b9f1dcaf0a135c44e48d48fd61d2f
Derek Thurn <derek@fake.email>
2025-06-04 08:24:08 -0700

    show cards on default stack position while prompt choice is being decided

--- rules_engine/src/display/src/rendering/position_overrides.rs
+++ rules_engine/src/display/src/rendering/position_overrides.rs
@@ -1,16 +1,24 @@
 use battle_state::battle::battle_state::BattleState;
 use battle_state::battle::card_id::CardId;
-use display_data::object_position::Position;
+use display_data::object_position::{Position, StackType};
 
 use crate::display_actions::apply_battle_display_action;
 
 /// Returns an alternate position for a card based on display logic, e.g.
 /// showing it in a browser.
-pub fn position(_battle: &BattleState, _card_id: CardId, position: Position) -> Position {
-    if let Some(browser_source) = apply_battle_display_action::current_browser_source() {
-        if position == browser_source {
-            return Position::Browser;
-        }
+pub fn position(battle: &BattleState, card_id: CardId, position: Position) -> Position {
+    if let Some(prompt) = &battle.prompt
+        && let Some(source_card_id) = prompt.source.card_id()
+        && source_card_id == card_id
+    {
+        return Position::OnStack(StackType::Default);
     }
+
+    if let Some(browser_source) = apply_battle_display_action::current_browser_source()
+        && position == browser_source
+    {
+        return Position::Browser;
+    }
+
     position
 }
