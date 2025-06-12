6c4759c8b8b7615ef5a7296f58b71e3461285824
Derek Thurn <derek@fake.email>
2025-06-12 05:57:26 -0700

    shrink mobile stack when it is big

--- client/Assets/Dreamtides/Layout/StackingObjectLayout.cs
+++ client/Assets/Dreamtides/Layout/StackingObjectLayout.cs
@@ -7,18 +7,32 @@ namespace Dreamtides.Layout
   public class StackingObjectLayout : StandardObjectLayout
   {
     [SerializeField] float _offset;
+    [SerializeField] float _shrinkOffset;
+    [SerializeField] int _shrinkOffsetThreshold;
     [SerializeField] bool _stackRight;
 
     protected override Vector3 CalculateObjectPosition(int index, int count)
     {
       var xOffset = _stackRight
-          ? index * _offset
-          : (index - (count - 1)) * _offset;
+          ? index * GetOffset(count)
+          : (index - (count - 1)) * GetOffset(count);
       return new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z);
     }
 
     protected override Vector3? CalculateObjectRotation(int index, int count) => transform.rotation.eulerAngles;
 
     protected override float? CalculateObjectScale(int index, int count) => transform.localScale.x;
+
+    float GetOffset(int count)
+    {
+      if (count < _shrinkOffsetThreshold)
+      {
+        return _offset;
+      }
+      else
+      {
+        return _shrinkOffset - (count - _shrinkOffsetThreshold) * 0.05f;
+      }
+    }
   }
 }
