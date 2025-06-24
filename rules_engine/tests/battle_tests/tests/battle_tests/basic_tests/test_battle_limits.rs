5a55da77f3f218549b78e64908c40ea5a575276b
Derek Thurn <derek@fake.email>
2025-06-24 06:12:54 -0700

    test character limit

--- rules_engine/tests/battle_tests/tests/battle_tests/basic_tests/test_battle_limits.rs
+++ rules_engine/tests/battle_tests/tests/battle_tests/basic_tests/test_battle_limits.rs
@@ -27,11 +27,39 @@ fn hand_size_limit_exceeded_gains_energy() {
     assert_eq!(
         s.user_client.me.energy(),
         initial_energy - draw_cost - Energy(1),
-        "User should have spent dreamscatter cost but gained 1 energy from hand size limit"
+        "User should have spent dreamscatter cost and 2 energy, then gained 1 energy from hand size limit"
     );
     assert_eq!(
         s.user_client.cards.user_hand().len(),
         10,
         "User should have drawn 1 card due to hand size limit"
+    );
+}
+
+#[test]
+fn character_limit_exceeded_abandons_character() {
+    let mut s = TestBattle::builder().connect();
+    let initial_void = s.user_client.cards.user_void().len();
+    // Add 8  to the battlefield
+    for _ in 0..8 {
+        s.add_to_battlefield(DisplayPlayer::User, CardName::MinstrelOfFallingLight);
+    }
+    assert_eq!(
+        s.user_client.cards.user_battlefield().len(),
+        8,
+        "User should have 8 characters on battlefield"
+    );
+    let char_id = s.add_to_hand(DisplayPlayer::User, CardName::MinstrelOfFallingLight);
+    s.play_card_from_hand(DisplayPlayer::User, &char_id);
+
+    assert_eq!(
+        s.user_client.cards.user_battlefield().len(),
+        8,
+        "User should still have 8 characters on battlefield"
+    );
+    assert_eq!(
+        s.user_client.cards.user_void().len(),
+        initial_void + 1,
+        "User void should have increased by 1"
     );
 }
