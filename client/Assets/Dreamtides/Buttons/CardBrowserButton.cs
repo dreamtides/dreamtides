65b6079bc57f98f19ddac80823c22f52993428b5
Derek Thurn <derek@fake.email>
2025-10-06 16:13:16 -0700

    prevent clicks on empty card browsers

--- client/Assets/Dreamtides/Buttons/CardBrowserButton.cs
+++ client/Assets/Dreamtides/Buttons/CardBrowserButton.cs
@@ -3,6 +3,7 @@
 using Dreamtides.Layout;
 using Dreamtides.Schema;
 using Dreamtides.Services;
+using Dreamtides.Utils;
 using UnityEngine;
 
 namespace Dreamtides.Components
@@ -19,7 +20,7 @@ namespace Dreamtides.Components
 
     public override void MouseUp(bool isSameObject)
     {
-      if (!isSameObject)
+      if (!isSameObject || IsEmpty())
       {
         return;
       }
@@ -33,6 +34,27 @@ namespace Dreamtides.Components
         },
       };
       _registry.ActionService.PerformAction(action);
+    }
+
+    bool IsEmpty()
+    {
+      switch (_type)
+      {
+        case CardBrowserType.UserDeck:
+          return _registry.Layout.UserDeck.Objects.Count == 0;
+        case CardBrowserType.EnemyDeck:
+          return _registry.Layout.EnemyDeck.Objects.Count == 0;
+        case CardBrowserType.UserVoid:
+          return _registry.Layout.UserVoid.Objects.Count == 0;
+        case CardBrowserType.EnemyVoid:
+          return _registry.Layout.EnemyVoid.Objects.Count == 0;
+        case CardBrowserType.UserStatus:
+          return false;
+        case CardBrowserType.EnemyStatus:
+          return false;
+        default:
+          throw Errors.UnknownEnumValue(_type);
+      }
     }
   }
 }
